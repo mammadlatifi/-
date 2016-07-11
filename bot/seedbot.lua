56e410db6098bee8361b6a0f4c85f99c01f84e94
mammadlatifi <mammadlatifi@users.noreply.github.com>
2016-07-11 23:16:47 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "set",
     "get",
     "broadcast",
-    "download_media",
+    "download_media",sudo_users={@dash_mammad}
     "invite",
     "all",
     "leave_ban",
